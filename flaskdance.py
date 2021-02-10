20f75413903bad3975117463ea396e8b1816cadf
teslatickles <hunterhartline@gmail.com>
2021-02-09 20:22:24 -0600

    add simple flask http api and esp32 wifi http server

---
+++ flaskdance.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/light/on', methods=['POST'])
+def lightOn():
+    print(request.data)
+    return str(request.data)
+
+@app.route('/accel', methods=['POST', 'GET'])
+def accel():
+    if request.method == 'POST':
+       print(request.data + "\n")
+       return str(request.data)
+
+@app.route('/gyro', methods=['POST', 'GET'])
+def gyro():
+    if request.method == 'POST':
+       print(request.data + "\n")
+       return str(request.data)
+
+@app.route('/magneto', methods=['POST', 'GET'])
+def magneto():
+    if request.method == 'POST':
+       print(request.data + "\n")
+       return str(request.data)
+
+@app.route('/temp', methods=['POST', 'GET'])
+def temp():
+    if request.method == 'POST':
+       print(request.data + "\n")
+       return str(request.data)
+
+@app.route('/power')
+app.run(host='0.0.0.0', port=8090)
---
+++ waifufaifu.ino
@@ -0,0 +1,37 @@
+#include <Wifi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "";
+const char* pwd = "";
+
+void setup() {
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, pwd)
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WaifuFaifu...");
+    }
+
+    Serial.println("Connected to WiFi network");
+}
+
+void loop() {
+    if ((WiFi.status() == WL_CONNECTED)) {
+        HTTPClient http;
+
+        http.begin("http://192.168.1.54:8090/button");
+        int httpCode = http.GET();
+
+        if (httpCode > 0) {
+            String payload = http.getString();
+            Serial.println(httpCode);
+            Serial.println(payload);
+        } else {
+            Serial.println("Error during HTTP request!");
+        }
+        http.end();
+    }
+    delay(20000);
+}
